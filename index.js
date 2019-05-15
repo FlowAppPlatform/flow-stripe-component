72244642b56126ced5b9056d365a03ce990b0349
Nnanyielugo <kz.onyegbule@gmail.com>
2019-05-15 11:03:35 +0100

    update default export

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 require('@babel/register');
 require('@babel/polyfill');
+
+const AddCard = require('./src/add-card');
+const AddPlan =require('./src/add-plan');
+const CancelSubscription =require('./src/cancel-subscription');
+const Charge =require('./src/charge');
+const CreateCustomer =require('./src/create-customer');
+const CreateSubscription =require('./src/create-subscription');
+const DeleteCard =require('./src/delete-card');
+const DeleteCustomer =require('./src/delete-customer');
+const DeletePlan =require('./src/delete-plan');
+
+module.exports = {
+  AddCard,
+  AddPlan,
+  CancelSubscription,
+  Charge,
+  CreateCustomer,
+  CreateSubscription,
+  DeleteCard,
+  DeleteCustomer,
+  DeletePlan
+};
